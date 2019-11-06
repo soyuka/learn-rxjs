aaec6d8451674f4885a29f2bb09504422c75d120
soyuka <soyuka@users.noreply.github.com>
2019-11-06 12:57:55 +0100

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,70 @@
-// RxJS v6+
-import { fromEvent, of } from 'rxjs';
+import './style.css';
+
+import { Observable, of, empty, fromEvent, from } from 'rxjs';
 import {
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  switchMap,
-  tap
+  delay,
+  switchMapTo,
+  concatAll,
+  count,
+  scan,
+  withLatestFrom,
+  share
 } from 'rxjs/operators';
 
-const getContinents = (keys: string) =>
-  [
-    'africa',
-    'antarctica',
-    'asia',
-    'australia',
-    'europe',
-    'north america',
-    'south america'
-  ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
+const requestOne = of('first').pipe(delay(500));
+const requestTwo = of('second').pipe(delay(800));
+const requestThree = of('third').pipe(delay(1100));
+const requestFour = of('fourth').pipe(delay(1400));
+const requestFive = of('fifth').pipe(delay(1700));
 
-const fakeContinentsRequest = (keys: string) =>
-  of(getContinents(keys)).pipe(
-    tap(_ => console.log(`API CALL at ${new Date()}`))
-  );
+const loadButton = document.getElementById('load');
+const progressBar = document.getElementById('progress');
+const content = document.getElementById('data');
 
-fromEvent(document.getElementById('type-ahead'), 'keyup')
-  .pipe(
-    debounceTime(200),
-    map((e: any) => e.target.value),
-    distinctUntilChanged(),
-    switchMap(fakeContinentsRequest),
-    tap(c => (document.getElementById('output').innerText = c.join('\n')))
-  )
-  .subscribe();
+// update progress bar as requests complete
+const updateProgress = (progressRatio: number) => {
+  console.log('Progress Ratio: ', progressRatio);
+  progressBar.style.width = 100 * progressRatio + '%';
+  if (progressRatio === 1) {
+    progressBar.className += ' finished';
+  } else {
+    progressBar.className = progressBar.className.replace(' finished', '');
+  }
+};
+// simple helper to log updates
+const updateContent = (newContent: string) => {
+  content.innerHTML += newContent;
+};
+
+const displayData = (data: string) => {
+  updateContent(`<div class="content-item">${data}</div>`);
+};
+
+// simulate 5 separate requests that complete at variable length
+const observables: Array<Observable<string>> = [
+  requestOne,
+  requestTwo,
+  requestThree,
+  requestFour,
+  requestFive
+];
+
+const array$ = from(observables);
+const requests$ = array$.pipe(concatAll());
+const clicks$ = fromEvent(loadButton, 'click');
+
+const progress$ = clicks$.pipe(
+  switchMapTo(requests$),
+  share()
+);
+
+const count$ = array$.pipe(count());
+
+const ratio$ = progress$.pipe(
+  scan(current => current + 1, 0),
+  withLatestFrom(count$, (current, count) => current / count)
+);
+
+clicks$.pipe(switchMapTo(ratio$)).subscribe(updateProgress);
+
+progress$.subscribe(displayData);
