0b144e3a1bf774a742c7b8482e0ce954d45d57a0
soyuka <soyuka@users.noreply.github.com>
2019-11-06 13:03:30 +0100

    save

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,73 @@
-import './style.css';
-
-import { Observable, of, empty, fromEvent, from } from 'rxjs';
+import { fromEvent, of, merge, empty, concat, defer } from 'rxjs';
 import {
   delay,
-  switchMapTo,
-  concatAll,
-  count,
-  scan,
-  withLatestFrom,
-  share
+  map,
+  mergeMap,
+  tap,
+  debounceTime,
+  distinctUntilChanged,
+  mapTo,
+  filter,
+  share,
+  switchAll
 } from 'rxjs/operators';
+import { format } from 'date-fns';
 
-const requestOne = of('first').pipe(delay(500));
-const requestTwo = of('second').pipe(delay(800));
-const requestThree = of('third').pipe(delay(1100));
-const requestFour = of('fourth').pipe(delay(1400));
-const requestFive = of('fifth').pipe(delay(1700));
+// track in progress saves
+let savesInProgress = 0;
 
-const loadButton = document.getElementById('load');
-const progressBar = document.getElementById('progress');
-const content = document.getElementById('data');
+// references
+const input = document.getElementById('note-input');
+const saveIndicator = document.querySelector('.save-indicator');
 
-// update progress bar as requests complete
-const updateProgress = (progressRatio: number) => {
-  console.log('Progress Ratio: ', progressRatio);
-  progressBar.style.width = 100 * progressRatio + '%';
-  if (progressRatio === 1) {
-    progressBar.className += ' finished';
-  } else {
-    progressBar.className = progressBar.className.replace(' finished', '');
-  }
-};
-// simple helper to log updates
-const updateContent = (newContent: string) => {
-  content.innerHTML += newContent;
+// streams
+const keyup$ = fromEvent(input, 'keyup');
+
+// fake save request
+const saveChanges = (value: string) => {
+  return of(value).pipe(delay(1500));
 };
 
-const displayData = (data: string) => {
-  updateContent(`<div class="content-item">${data}</div>`);
-};
-
-// simulate 5 separate requests that complete at variable length
-const observables: Array<Observable<string>> = [
-  requestOne,
-  requestTwo,
-  requestThree,
-  requestFour,
-  requestFive
-];
-
-const array$ = from(observables);
-const requests$ = array$.pipe(concatAll());
-const clicks$ = fromEvent(loadButton, 'click');
-
-const progress$ = clicks$.pipe(
-  switchMapTo(requests$),
+/**
+ * Trigger a save when the user stops typing for 200ms
+ * After new data has been successfully saved, so a saved
+ * and last updated indicator.
+ */
+const inputToSave$ = keyup$.pipe(
+  debounceTime(200),
+  map(e => (e.target as any).value),
+  distinctUntilChanged(),
   share()
 );
 
-const count$ = array$.pipe(count());
-
-const ratio$ = progress$.pipe(
-  scan(current => current + 1, 0),
-  withLatestFrom(count$, (current, count) => current / count)
+const savesInProgress$ = inputToSave$.pipe(
+  mapTo(of('Saving')),
+  tap(_ => savesInProgress++)
 );
 
-clicks$.pipe(switchMapTo(ratio$)).subscribe(updateProgress);
+const savesCompleted$ = inputToSave$.pipe(
+  mergeMap(saveChanges),
+  tap(_ => savesInProgress--),
+  // ignore if additional saves are in progress
+  filter(_ => !savesInProgress),
+  mapTo(
+    concat(
+      // display saved for 2s
+      of('Saved!'),
+      empty().pipe(delay(2000)),
+      // then last updated time, defer for proper time
+      defer(() => of(`Last updated: ${format(Date.now(), 'MM/DD/YYYY hh:mm')}`))
+    )
+  )
+);
 
-progress$.subscribe(displayData);
+merge(savesInProgress$, savesCompleted$)
+  .pipe(
+    /*
+   If new save comes in when our completion observable is running, we want to switch to it for a status update.
+  */
+    switchAll()
+  )
+  .subscribe(status => {
+    saveIndicator.innerHTML = status;
+  });
