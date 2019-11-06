1c2d0861bc87a6fdcf2dce09553e75483afcf94f
soyuka <soyuka@users.noreply.github.com>
2019-11-06 11:57:30 +0100

    #1

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,15 @@
 import './style.css'
-import { fromEvent, merge } from 'rxjs'
-import { map, buffer, throttleTime, filter } from 'rxjs/operators'
+import { fromEvent } from 'rxjs'
+import { map, scan } from 'rxjs/operators'
 
 var button = document.querySelector('.this');
 var click$ = fromEvent(button, 'click');
 
+var stream = click$.pipe(
+    map(e => 1),
+    scan((acc, curr) => acc + curr, 0)
+)
 
-// HERE
-// The 4 lines of code that make the multi-click logic
-// var multiClickStream = click$
-//   .pipe(
-//     buffer(function() { return click$.throttle(250); }),
-//     map(function(list) { return list.length; }),
-//     filter(function(x) { return x >= 2; })
-//   )
-
-// Same as above, but detects single clicks
-var singleClickStream = click$
-  .pipe(
-    buffer(click$.pipe(throttleTime(250))),
-    map(list => list.length),
-    filter(x => x === 1)
-  )
-
-// Listen to both streams and render the text label accordingly
-singleClickStream.subscribe(function (event) {
-    document.querySelector('h2').textContent = 'click';
-});
-// multiClickStream.subscribe(function (numclicks) {
-//     document.querySelector('h2').textContent = ''+numclicks+'x click';
-// });
-
-// merge(singleClickStream, multiClickStream)
-//     .throttle(1000)
-//     .subscribe(function (suggestion) {
-//         document.querySelector('h2').textContent = '';
-//     });
-
-
+stream.subscribe((count) => {
+  document.querySelector('h2').textContent = `${count} times`;
+})
